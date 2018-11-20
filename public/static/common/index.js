da03a01c212d901fd6884ad328fff74f36ad0222
汤圆 <272495222@qq.com>
2018-11-20 16:20:05 +0800

    提交样式

---
+++ public/static/common/index.js
@@ -0,0 +1,39 @@
+$(function(){
+
+
+  $('.yj-nav-a').hover(function(){
+     $('.yj-nav-child',this).stop(false,true).slideDown('fast');
+  },function(){
+     $('.yj-nav-child',this).stop(false,true).slideUp('fast');
+  });
+
+
+  // 退出
+  $('#loginOut').on('click', function(event) {
+      $.ajax({
+        url: '/index.php/index/LoginIn/login',
+        data: {
+          code : 1
+        },
+        dataType: 'json',
+        type: 'POST',
+        cache: false,
+        beforeSend: function() 
+        {
+          
+        },
+        success: function(ret) 
+        {
+          console.log(ret);
+          location.href = "/index.php/index/index"
+        },    
+        error: function() 
+        {
+          
+        },    
+      });
+  });
+});
+
+
+
